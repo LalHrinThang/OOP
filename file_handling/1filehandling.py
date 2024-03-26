068fa7e502385f658b8fe25b02bdde18577304e7
LalHrinThang <lalhrinthang@gmail.com>
2024-03-26 14:29:27 +0800

    write method to over-written the contents

--- file_handling/1filehandling.py
+++ file_handling/1filehandling.py
@@ -6,8 +6,14 @@ if file:
     print("Success of Opening file")
     data = file.read(16) # -1 == () will read the whole contents
     print(data)
+    data2 = file2.write("Hello This is Me Lal Hrin Thang") 
+    # the previous content was Hello This is Me
+    
+
+    print(f"This is with write method {data2}")
+
+    # data3 = file3.write(" : This is appended message")
+    # print(f"This is with append method {data3}")
+    file.close()
 else:
     print("There is no file")
-
-
-file.close()
