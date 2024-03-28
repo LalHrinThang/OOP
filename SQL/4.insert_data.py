20879223dc7e6de312bcd97cdc5c64e78d7cbd0d
LalHrinThang <lalhrinthang@gmail.com>
2024-03-28 23:06:26 +0800

    insert data into Table

--- SQL/4.insert_data.py
+++ SQL/4.insert_data.py
@@ -11,10 +11,16 @@ dbConnection = mysql.connector.connect(
 dbcursor = dbConnection.cursor()
 #dbcursor.execute("CREATE TABLE students(id int primary key AUTO_INCREMENT,name VARCHAR(30),age SMALLINT,class TINYINT)")
 
-dbcursor.execute("ALTER TABLE students ADD hobby VARCHAR(30)")
+#dbcursor.execute("ALTER TABLE students ADD hobby VARCHAR(30)")
 
+insert_data_type= "INSERT INTO students (id,name,age,class,hobby)values(%s,%s,%s,%s,%s)"
 
-dbcursor.execute("DESCRIBE students")
+insert_data = [(101,"Lal Hrin Thang",24,12,"Programming"),(102,"Lal Hrin Thang",24,12,"Programming"),(103,"Lal Hrin Thang",24,12,"Programming")]
+dbcursor.executemany(insert_data_type,insert_data)
 
+print("Data successfully inserted")
+print(dbcursor.rowcount)
+
+dbcursor.execute("SELECT * FROM students")
 for i in dbcursor:
     print(i)
