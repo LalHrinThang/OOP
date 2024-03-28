14b23944b0bcca932b8b5a8879c601ce81726ca3
LalHrinThang <lalhrinthang@gmail.com>
2024-03-28 22:57:52 +0800

    Inserting Table to previous database

---
+++ SQL/4.insert_data.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+dbConnection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "004962",
+    database = "TableCreation"
+)
+
+
+dbcursor = dbConnection.cursor()
+dbcursor.execute("CREATE TABLE students(id int primary key AUTO_INCREMENT,name VARCHAR(30),age SMALLINT,class TINYINT)")
+
+#dbcursor.execute("ALTER TABLE Table ADD hobby VARCHAR(30)")
+
+
+dbcursor.execute("DESCRIBE students")
+
+for i in dbcursor:
+    print(i)
