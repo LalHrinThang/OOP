9d82acbb5d16ef0dd76745ed5bb8116d38527110
LalHrinThang <lalhrinthang@gmail.com>
2024-03-28 16:28:22 +0800

    Create Table

--- SQL/3.create_table.py
+++ SQL/3.create_table.py
@@ -3,13 +3,13 @@ import mysql.connector
 myDB_connection = mysql.connector.connect(
     host ="localhost",
     user ="root",
-    passwd ="004962"
+    passwd ="004962",
+    database ="TableCreation"
 )
 
 myCursor = myDB_connection.cursor()
 
-myCursor.execute("CREATE DATABASE TableCreation")
-myCursor.execute("SHOW DATABASES")
+#myCursor.execute("CREATE TABLE FirstTB(id int primary key AUTO_INCREMENT,name VARCHAR(30),age SMALLINT,class TINYINT)")
 
 for database in myCursor:
     print(database)
