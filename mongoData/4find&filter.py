33a0238a477fe1b1981cb9398d53ec5dd924e4bd
LalHrinThang <lalhrinthang@gmail.com>
2024-03-29 18:47:46 +0800

    insert many

--- mongoData/4find&filter.py
+++ mongoData/4find&filter.py
@@ -7,9 +7,15 @@ database = connection["FirstDB"]
 collection = database["Students_Info"]
 
 
-data = {"_id" : 4,"name" : "Benjamin Aaron" , "age":22,"class":12}
+data = [
+    {"_id" : 5,"name" : "Benny" , "age":22,"class":13},
+    {"_id" : 6,"name" : "Zane" , "age" : 22, "class": 14},
+    {"_id" : 7,"name" : "Benjamin Lala","age" : 15,"class": 12}
+]
 
 try:
+    # result = collection.insert_many(data)
+    # print(result.inserted_id)
     _ids = collection.find().distinct("_id")
 
     print(_ids)
