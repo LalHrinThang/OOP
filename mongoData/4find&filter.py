95d7c8a1559ab84b01a4b2619b455247bc2f58db
LalHrinThang <lalhrinthang@gmail.com>
2024-03-29 18:53:17 +0800

    find method()

--- mongoData/4find&filter.py
+++ mongoData/4find&filter.py
@@ -16,11 +16,28 @@ data = [
 try:
     # result = collection.insert_many(data)
     # print(result.inserted_id)
-    _ids = collection.find().distinct("_id")
+    # _ids = collection.find().distinct("_id")
 
-    print(_ids)
-    _name = collection.find().distinct("name")
-    print(_name)
+    # print(_ids)
+    # _name = collection.find().distinct("name")
+    # print(_name)
+
+    data_finding = collection.find_one()
+    print(data_finding)
+    print("This is find_one method \n")
+    data_finding = collection.find()
+    for i in data_finding:
+        print(i)
+    print("This is find(all) method\n")
+    data_finding = collection.find({},{"name":0})
+    for j in data_finding:
+        print(j)
+    print("This is selective find method \n")
+
+    data_finding = collection.find({},{"name":1})
+    for k in data_finding:
+        print(k)
+    print("\n This is selective finding method")
 except Exception as error:
     print(error)
 
