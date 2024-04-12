26907255e283e307522dd27efa36ca996b28c726
lalhrinthang <lalhrinthang@gmail.com>
2024-04-12 17:16:17 +0800

    connect with urls.py to execute command

--- django_project/project_todo/todo_app/views.py
+++ django_project/project_todo/todo_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def todo_list(request):
+    return HttpResponse("This is from todo_list function")
