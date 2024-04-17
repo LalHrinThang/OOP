e4e9432534699bda8aa8011c83463db15d289a67
LalHrinThang <lalhrinthang@gmail.com>
2024-04-17 23:06:11 +0800

    first django project finished

--- django_project/project_todo/todo_app/views.py
+++ django_project/project_todo/todo_app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpRequest
+from .models import Todo_app # model.py from 
 
 # Create your views here.
 def todo_list(request):
-    return HttpResponse("This is from todo_list function")
+    all_data = {'todo_list' : Todo_app.objects.all()}
+    return render(request,'todo_app/todo.html',all_data)
+
+
+def insert_todo(request:HttpRequest):
+    todo = Todo_app(content = request.POST['content'])
+    todo.save()
+    return redirect('/todo/list')
+
+def delete_todo_item(request,todo_id):
+    delete_id = Todo_app.objects.get(id=todo_id)
+    delete_id.delete()
+    return redirect('/todo/list')
